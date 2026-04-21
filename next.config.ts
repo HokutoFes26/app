365b8d0c0d49292914dd838ff2faf797a404fefd
rsu <61874914+rsu-Suba@users.noreply.github.com>
2026-04-21 23:18:48 +0900

    Fix: Path fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+const SUB_DIRECTORY = "/app";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
   trailingSlash: true,
+  basePath: isProd ? SUB_DIRECTORY : "",
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : "",
+  },
   images: {
     unoptimized: true,
   },
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,7 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
-export const BASE_PATH = "";
+export const BASE_PATH = isProd ? "/app" : "";
 
 export const getPath = (path: string): string => {
+    if (path.startsWith(BASE_PATH)) return path;
     const normalizedPath = path.startsWith("/") ? path : `/${path}`;
     return `${BASE_PATH}${normalizedPath}`;
 };
